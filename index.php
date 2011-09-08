7c2aa62a095329ed34c5f06438fff8c9aeb68582
Pedro Belo <pedro@heroku.com>
2011-09-07 19:42:46 -0700

    added heroku layout

--- index.php
+++ index.php
@@ -46,9 +46,9 @@ if ($token) {
     idx(FBUtils::fetchFromFBGraph("me/likes?access_token=$token&limit=11"), 'data')
   );
 
-  // This fetches 5 of your friends.
+  // This fetches 3 of your friends.
   $friends = array_values(
-    idx(FBUtils::fetchFromFBGraph("me/friends?access_token=$token&limit=5"), 'data')
+    idx(FBUtils::fetchFromFBGraph("me/friends?access_token=$token&limit=3"), 'data')
   );
 
   // And this returns 2 of your photos.
@@ -86,11 +86,15 @@ if ($token) {
 <!-- default will remove any html tags from the value being      -->
 <!-- and echoEntity() will echo the sanitized content.  Both of  -->
 <!-- these functions are located and documented in 'utils.php'.  -->
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
+    <meta charset="utf-8">
+
     <!-- We get the name of the app out of the information fetched -->
     <title><?php echo(idx($app_info, 'name')) ?></title>
-    <link type="text/css" rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="stylesheets/screen.css" media="screen">
+
     <!-- These are Open Graph tags.  They add meta data to your  -->
     <!-- site that facebook uses when your content is shared     -->
     <!-- over facebook.  You should fill these tags in with      -->
@@ -115,97 +119,70 @@ if ($token) {
   </script>
   </head>
   <body>
-  <div class="content">
-    <!--  Display the app name and user's name from the data fetched -->
-    <h2>
-      Welcome to
-      <?php echo(idx($app_info, 'name') . ', ' . idx($basic, 'name') . '.'); ?>
-    </h2>
-    <table cellspacing="30">
-      <tr>
-        <td valign="top">
-          <h3> Your Picture: </h3>
-          <!-- By passing a valid access token here, we are able to display -->
-          <!-- the user's images without having to download or prepare -->
-          <!-- them ahead of time -->
-          <a
-            href="#"
-            onclick="window.open('http://www.facebook.com/<?php echo($my_id) ?>')">
-            <img
-              src="https://graph.facebook.com/me/picture?type=large&access_token=<?php echoEntity($token) ?>"
-              width="200"
-            />
-          </a>
-        </td>
-        <td valign="top">
-          <h3> Friends using this app: </h3>
-          <table border="0">
-            <?php
-              foreach ($app_using_friends as $auf) {
-                // Extract the pieces of info we need from the requests above
-                $uid = assertNumeric(idx($auf, 'uid'));
-                $pic = idx($auf, 'pic_square');
-                $name = idx($auf, 'name');
-                echo('
-                  <tr>
-                    <td>
-                      <a
-                        href="#"
-                        onclick="window.open(\'http://www.facebook.com/' . $uid . '\')">
-                        <img src=' .$pic . '/>
-                      </a>
-                    </td>
-                    <td> <p>' . $name . '</p> </td>
-                  <tr>');
-              }
-            ?>
-          </table>
-        </td>
-        <td valign="top">
-          <h3> Share your app: </h3>
-          <p>
-            <!-- Here we use the link for the 'Send Dialog' from earlier-->
-            <a
-              href="#"
-              onclick="popup('<?php echo $send_url ?>', 'Send', 580, 400);">
-              Send your app to your friends
-            </a>
-          </p>
-          <p>
-            <!-- This displays the other dialog link, which allows users -->
-            <!-- to share this app on their wall -->
-            <a
-              href="#"
-              onclick="popup('<?php echo $post_to_wall_url ?>', 'Post to Wall', 580, 400);">
-              Post to your wall
-            </a>
-          </p>
-        </td>
-        <td valign="top">
-          <h3> Getting Started Guides: </h3>
-          <p>
-            <a href="#" onclick="window.open('http://developers.facebook.com/docs/guides/web/')">
-              <b>Websites: </b>
-              Drive growth and engagement on your site through Facebook Login and Social Plugins.
-            </a>
-          </p>
-          <p>
-            <a href="#" onclick="window.open('http://developers.facebook.com/docs/guides/canvas/')">
-              <b>Apps on Facebook: </b>
-              Integrate with our core experience by building apps that operate within Facebook.
-            </a>
-          </p>
-          <p>
-            <a href="#" onclick="window.open('http://developers.facebook.com/docs/guides/mobile/')">
-              <b> Mobile: </b> 
-              Let users find and connect to their friends in mobile apps and games.
-            </a>
-          </p>
-        </td>
-      </tr>
-      <tr>
-        <td valign="top">
-          <h3> Recent Photos: </h3>
+    <header class="clearfix">
+      <!-- By passing a valid access token here, we are able to display -->
+      <!-- the user's images without having to download or prepare -->
+      <!-- them ahead of time -->
+      <a
+        href="#"
+        onclick="window.open('http://www.facebook.com/<?php echo($my_id) ?>')">
+        <img
+          src="https://graph.facebook.com/me/picture?type=large&access_token=<?php echoEntity($token) ?>"
+          height="64"
+        />
+      </a>
+
+      <div>
+        <h1>Welcome, <strong><?php echo idx($basic, 'name'); ?></strong></h1>
+        <p class="tagline">This app is named <?php echo(idx($app_info, 'name')); ?></p>
+        <div id="share-app">
+          <p>Share your app:</p>
+          <ul>
+            <li>
+              <a href="#" class="facebook-button" onclick="popup('<?php echo $post_to_wall_url ?>', 'Post to Wall', 580, 400);">
+                <span class="plus">Post to Wall</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="facebook-button speech-bubble" onclick="popup('<?php echo $send_url ?>', 'Send', 580, 400);">
+                <span class="speech-bubble">Send to Friends</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    <section id="get-started">
+      <p>Welcome to your Facebook app, running on <span>heroku</span>!</p>
+      <a href="http://devcenter.heroku.com/articles/facebook" class="button">Learn how to edit this app</a>
+    </section>
+
+    <section id="samples" class="clearfix">
+      <h1>Examples of the Facebook Graph API</h1>
+
+      <div class="list">
+        <h3>A few of your friends</h3>
+        <ul class="friends">
+          <?php
+            foreach ($friends as $friend) {
+              // Extract the pieces of info we need from the requests above
+              $id = assertNumeric(idx($friend, 'id'));
+              $name = idx($friend, 'name');
+              // Here we link each friend we display to their profile
+              echo('
+                <li style="background-image: url(https://graph.facebook.com/' . $id . '/picture)">
+                  <a href="#" onclick="window.open(\'http://www.facebook.com/' . $id . '\')">
+                      ' . $name . '
+                  </a>
+                </li>');
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="list inline">
+        <h3>Recent photos</h3>
+        <ul class="photos">
           <?php
             foreach ($photos as $photo) {
               // Extract the pieces of info we need from the requests above
@@ -214,69 +191,86 @@ if ($token) {
 
               // Here we link each photo we display to it's location on Facebook
               echo('
-                <p>
-                  <a
-                    href="#"
-                    onclick="window.open(\'http://www.facebook.com/' .$id . '\')">
-                    <img src="' .$src . '" width="200" />
+                <li>
+                  <a href="#" onclick="window.open(\'http://www.facebook.com/' .$id . '\')">
+                    <img src="' .$src . '" width="163" />
                   </a>
-                </p>'
+                </li>'
               );
             }
           ?>
-        </td>
-        <td valign="top">
-          <h3> A few of your friends: </h3>
-          <table border="0">
-            <?php
-              foreach ($friends as $friend) {
-                // Extract the pieces of info we need from the requests above
-                $id = assertNumeric(idx($friend, 'id'));
-                $name = idx($friend, 'name');
-                // Here we link each friend we display to their profile
-                echo('
-                  <tr>
-                    <td>
-                      <a
-                        href="#"
-                        onclick="window.open(\'http://www.facebook.com/' . $id . '\')">
-                        <img src="https://graph.facebook.com/' . $id . '/picture"/>
-                      </a>
-                    </td>
-                    <td> <p>' . $name . '</p> </td>
-                  <tr>'
-                );
-              }
-            ?>
-          </table>
-        </td>
-        <td valign="top">
-          <h3> Things you like: </h3>
-          <ul>
-            <?php
-              foreach ($likes as $like) {
-                // Extract the pieces of info we need from the requests above
-                $id = assertNumeric(idx($like, 'id'));
-                $item = idx($like, 'name');
-                // This display's the object that the user liked as a link to
-                // that object's page.
-                echo('
-                  <li>
-                    <p>
-                      <a
-                        href="#"
-                        onclick="window.open(\'http://www.facebook.com/' .$id .'\')">' .
-                        $item .
-                      '</a>
-                    </p>
-                  </li>'
-                );
-              }
-            ?>
-          </ul>
-        </td>
-      </tr>
-    <table>
-  </div>
+        </ul>
+      </div>
+
+      <div class="list">
+        <h3>Things you like</h3>
+        <ul class="things">
+          <?php
+            foreach ($likes as $like) {
+              // Extract the pieces of info we need from the requests above
+              $id = assertNumeric(idx($like, 'id'));
+              $item = idx($like, 'name');
+              // This display's the object that the user liked as a link to
+              // that object's page.
+              echo('
+                <li>
+                  <a href="#" onclick="window.open(\'http://www.facebook.com/' .$id .'\')">
+                    ' . $item . '
+                  </a>
+                </li>');
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="list">
+        <h3>Friends using this app</h3>
+        <ul class="friends">
+          <?php
+            foreach ($app_using_friends as $auf) {
+              // Extract the pieces of info we need from the requests above
+              $uid = assertNumeric(idx($auf, 'uid'));
+              $pic = idx($auf, 'pic_square');
+              $name = idx($auf, 'name');
+              echo('<li style="background-image: url(' .$pic . ')">' . $name . '</li>');
+            }
+          ?>
+          
+        </ul>
+      </div>
+    </section>
+
+    <section id="guides" class="clearfix">
+      <h1>Learn More About Heroku &amp; Facebook Apps</h1>
+      <ul>
+        <li>
+          <a href="http://www.heroku.com/" class="icon heroku">Heroku</a>
+          <p>Learn more about <a href="http://www.heroku.com/">Heroku</a>, or read developer docs in the Heroku <a href="http://devcenter.heroku.com/">Dev Center</a>.</p>
+        </li>
+        <li>
+          <a href="#" class="icon websites">Websites</a>
+          <p>
+            Drive growth and engagement on your site through
+            Facebook Login and Social Plugins.
+          </p>
+        </li>
+        <li>
+          <a href="#" class="icon mobile-apps">Mobile Apps</a>
+          <p>
+            Integrate with our core exper-ience by building apps
+            that operate within Facebook.
+          </p>
+        </li>
+        <li>
+          <a href="#" class="icon apps-on-facebook">Apps on Facebook</a>
+          <p>Let users find and connect to their friends in mobile apps and games.</p>
+        </li>
+      </ul>
+    </section>
+  </body>
+    
+    
+    
+    
   </body>
 </html>
