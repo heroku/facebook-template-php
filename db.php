2f700e5eaf6fe8bab3c280efec2a4ff1913d0049
Bhushan shah <bhush94@gmail.com>
2011-11-29 09:23:28 +0530

    Database connection demo added

---
+++ db.php
@@ -0,0 +1,28 @@
+<?php
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+
+# Here we establish the connection. Yes, that's all.
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+if(!pg_conn)
+{
+  echo("You have not added database yet from terminal issue following command");
+  echo("<code>heroku addons:add shared-database</code>");
+}
+
+# Now let's use the connection for something silly just to prove it works:
+$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+
+print "<pre>\n";
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+} else {
+  print "Tables in your database:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}
+print "\n";
+?>
