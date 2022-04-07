004c689398910f71a27cc813f01eafc7c9c728d6
stellajnolan <103118121+stellajnolan@users.noreply.github.com>
2022-04-06 22:49:12 -0700

    <br> tag added

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 echo "fuLL Text";
-echo "hello";
-echo "hello";
-echo "hello";
-echo "hello";echo "hello";echo "hello";
+echo "hello"."<br>";
+echo "hello"."<br>";
+echo "hello"."<br>";
+echo "hello"."<br>";
 for ($i = 1; $i <= 10; $i++) {
-    echo $i;
+    echo $i."<br>";
 }
